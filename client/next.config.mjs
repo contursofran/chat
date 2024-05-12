acd5f50bb1139b7ec52069b9b14a6c48b0c314b9
Franco Conturso <contursofran@gmail.com>
2024-05-11 23:18:31 -0300

    fix: double socket generation

---
+++ client/next.config.mjs
@@ -0,0 +1,16 @@
+// const nextConfig = {
+// webpack: (config) => {
+//     config.resolve.fallback = {
+//       ...config.resolve.fallback,
+//       net: false,
+//       os: false,
+//     };
+//     return config;
+//   },
+// };
+
+const nextConfig = {
+  reactStrictMode: false,
+};
+
+export default nextConfig;
